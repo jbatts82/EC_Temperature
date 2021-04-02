b7cf1aad61e03e230f12ddcb1a9e854074d6442f
jbatts82 <jbatts82@hotmail.com>
2021-04-02 09:04:14 -0400

    Update TheSensorApp.py
    Clean up control layer

--- TheSensorApp.py
+++ TheSensorApp.py
@@ -9,40 +9,35 @@ from time import sleep
 from config import Config
 from temp_sensing.DHT11 import DHT11
 from support import log
+from support import div
 from data.DB_App import DataBase_App
 from control.leds import leds
 from control.plug import KasaPlug
 
 if __name__ == '__main__':
-	print("###############################################################################")
 	log("Starting Main System", __file__)
-	
+	div()
 	the_config = Config()
 
 	# initialize database
-	print("###############################################################################")
+	div()
 	the_database = DataBase_App(the_config)
 
 	# initialize leds
-	print("###############################################################################")
+	div()
 	the_leds = leds(the_config)
 
 	# initialize temp sensing
-	print("###############################################################################")
 	sensor1 = DHT11(the_config)
-	print("###############################################################################")
 	sensor2 = DHT11(the_config)
-	print("###############################################################################")
 	sensor3 = DHT11(the_config)
-	print("###############################################################################")
 	sensor4 = DHT11(the_config)
+	sensor_array = [sensor1, sensor2, sensor3, sensor4]
 
-	# initialize plug controlers
-	print("###############################################################################")
-	plug1 = KasaPlug(the_config)
-	print("###############################################################################")
-	plug2 = KasaPlug(the_config)
-	print("###############################################################################")
-	plug3 = KasaPlug(the_config)
-	print("###############################################################################")
-	plug4 = KasaPlug(the_config)
+
+	# Run Sensors
+	while True:
+		for sensor in sensor_array:
+			div()
+			sensor.process_sensor()
+		sleep(10)
